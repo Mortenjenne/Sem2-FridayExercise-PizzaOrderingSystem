a9574b88990748cfc55791ee303f44189ff4442c
Morten Jensen <mo_ten@hotmail.com>
2025-09-07 11:31:20 +0200

    Fixed NoDiscount returning zero in total

--- src/main/java/pizza/discount/NoDiscount.java
+++ src/main/java/pizza/discount/NoDiscount.java
@@ -3,6 +3,6 @@ package pizza.discount;
 public class NoDiscount implements DiscountStrategy{
     @Override
     public double applyDiscount(double total) {
-        return total * 0.0;
+        return total;
     }
 }
